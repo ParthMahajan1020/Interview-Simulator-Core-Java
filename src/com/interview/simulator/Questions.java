d2ef4a158d2bb4bef3d8da867f3723c9d911b98b
Parth Mahajan <232033482+ParthMahajan1020@users.noreply.github.com>
2025-12-29 17:28:08 +0530

    Modify Questions constructor to include difficulty
    Added difficulty attribute to Questions class.

--- src/com/interview/simulator/Questions.java
+++ src/com/interview/simulator/Questions.java
@@ -4,11 +4,22 @@ public abstract class Questions {
 
     protected String questionText;
     protected int marks;
+    protected String difficulty; // MEDIUM / HARD
 
-    public Questions(String questionText, int marks) {
+    public Questions(String questionText, int marks, String difficulty) {
         this.questionText = questionText;
         this.marks = marks;
+        this.difficulty = difficulty;
     }
 
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public abstract void displayQuestion();
     public abstract int evaluateAnswer(String answer);
 }
